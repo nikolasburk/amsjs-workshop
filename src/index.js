8c1343be0da8c12e7338511c65b30ea82907bc20
Nikolas Burk <nikolas.burk@gmail.com>
2018-05-30 20:43:39 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API for a simple blogging application.`,
+    posts: (_, args, context, info) => {
+      return context.db.query.posts(
+        {
+          where: {
+            OR: [
+              {
+                title_contains: args.searchString,
+              },
+              {
+                content_contains: args.searchString,
+              },
+            ],
+          },
+        },
+        info,
+      )
+    },
+    post: (_, args, context, info) => {
+      return context.db.query.post(
+        {
+          where: {
+            id: args.id,
+          },
+        },
+        info,
+      )
+    },
+  },
+  Mutation: {
+    createDraft: (_, args, context, info) => {
+      return context.db.mutation.createPost(
+        {
+          data: {
+            title: args.title,
+            content: args.content,
+          },
+        },
+        info,
+      )
+    },
+    publish: (_, args, context, info) => {
+      return context.db.mutation.updatePost(
+        {
+          where: {
+            id: args.id,
+          },
+          data: {
+            published: true,
+          },
+        },
+        info,
+      )
+    },
+    deletePost: (_, args, context, info) => {
+      return context.db.mutation.deletePost(
+        {
+          where: {
+            id: args.id,
+          },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: './src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/nikolas-burk/prisma/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
+})
+server.start(() =>
+  console.log(`GraphQL server is running on http://localhost:4000`),
+)
